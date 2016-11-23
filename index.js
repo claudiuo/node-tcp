b38efd8683ac89a5e7b7c6efe214f3f650bb728f
coprea <coprea@attensity.com>
2016-11-23 13:47:43 -0700

    code to use args to turn lights on/off

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if(process.argv.length < 4) {
 }
 
 var room = process.argv[2];
-var stateDesired = process.argv[3];
+var stateDesired = process.argv[3].toLowerCase;
 
 if(stateDesired === "dim") {
 	// in this case, we need an extra argument
@@ -23,47 +23,56 @@ if(stateDesired === "dim") {
 	}
 }
 
-Test.Init(function(error){
-	if(!error){
-		console.log("all is well with the setup");
-	}else{
+
+// TODO try this code first!!!!
+Test.Init(function (error) {
+	if(!error) {
+		console.log("token is fine");
+		if(stateDesired == "on") {
+			console.log("trying to turn on " + room);
+			Test.TurnOnRoomByName(room, function (error) {
+				Test.GWEnd();
+			});
+		} else {
+			console.log("trying to turn off " + room);
+			Test.TurnOffRoomByName(room, function (error) {
+				Test.GWEnd();
+			});
+		}
+	} else {
 		console.log("There was an issue initializing the token");
 	}
 });
 
-if(stateDesired == "on"){
-	console.log("trying to turn on " + room);
-	Test.TurnOnRoomByName(room, function(error){
-		Test.GWEnd();
-	});
-}else{
-	console.log("trying to turn off " + room);
-	Test.TurnOffRoomByName(room, function(error){
-		Test.GWEnd();
-	});
-}
-
 // TODO: brightness
 
-
-// Test.Init(function(error){
-// 	if(!error){
-// 		Test.GetState(function(error,system){
-// 			// room = "Hallway";
-// 			Test.GetRoomStateByName(room, function(error,state,level){
-// 				console.log("State: " + state + " at Level: " + level);
-// 				if(state == 1){
-// 					Test.TurnOffRoomByName(room, function(error){
-// 						Test.GWEnd();
-// 					});
-// 				}else{
-// 					Test.TurnOnRoomByName(room, function(error){
-// 						Test.GWEnd();
-// 					});
+// TODO if above code not working, uncomment this block and try
+// Test.Init(function (error) {
+// 	if (!error) {
+// 		console.log("token is fine");
+// 		Test.GetState(function (error, system) {
+// 			Test.GetRoomStateByName(room, function (error, state, level) {
+// 				console.log("Room " + room + " - State " + state + " at Level " + level);
+// 				if (state == 1) {
+// 					// lights are on, if stateDesired is ON, nothing to do
+// 					// if stateDesired is OFF, turn lights off
+// 					if (stateDesired == "on") {
+// 						Test.TurnOffRoomByName(room, function (error) {
+// 							Test.GWEnd();
+// 						});
+// 					}
+// 				} else {
+// 					// lights are off, if stateDesired is OFF, nothing to do
+// 					// if stateDesired is ON, turn lights on
+// 					if (stateDesired == "on") {
+// 						Test.TurnOnRoomByName(room, function (error) {
+// 							Test.GWEnd();
+// 						});
+// 					}
 // 				}
 // 			});
 // 		});
-// 	}else{
+// 	} else {
 // 		console.log("There was an issue initializing the token");
 // 	}
 // });
