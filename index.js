fcc7bd4a019e69e143d5363c4890ef7f7601ac0a
claudiuo <claudiuo@gmail.com>
2016-11-23 17:57:22 -0700

    making code work for on/off

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if(process.argv.length < 4) {
 }
 
 var room = process.argv[2];
-var stateDesired = process.argv[3].toLowerCase;
+var stateDesired = process.argv[3].toLowerCase();
 
 if(stateDesired === "dim") {
 	// in this case, we need an extra argument
@@ -23,56 +23,59 @@ if(stateDesired === "dim") {
 	}
 }
 
+console.log("stateDesired " + stateDesired);
 
 // TODO try this code first!!!!
-Test.Init(function (error) {
-	if(!error) {
-		console.log("token is fine");
-		if(stateDesired == "on") {
-			console.log("trying to turn on " + room);
-			Test.TurnOnRoomByName(room, function (error) {
-				Test.GWEnd();
-			});
-		} else {
-			console.log("trying to turn off " + room);
-			Test.TurnOffRoomByName(room, function (error) {
-				Test.GWEnd();
-			});
-		}
-	} else {
-		console.log("There was an issue initializing the token");
-	}
-});
-
-// TODO: brightness
-
-// TODO if above code not working, uncomment this block and try
 // Test.Init(function (error) {
-// 	if (!error) {
+// 	if(!error) {
 // 		console.log("token is fine");
-// 		Test.GetState(function (error, system) {
-// 			Test.GetRoomStateByName(room, function (error, state, level) {
-// 				console.log("Room " + room + " - State " + state + " at Level " + level);
-// 				if (state == 1) {
-// 					// lights are on, if stateDesired is ON, nothing to do
-// 					// if stateDesired is OFF, turn lights off
-// 					if (stateDesired == "on") {
-// 						Test.TurnOffRoomByName(room, function (error) {
-// 							Test.GWEnd();
-// 						});
-// 					}
-// 				} else {
-// 					// lights are off, if stateDesired is OFF, nothing to do
-// 					// if stateDesired is ON, turn lights on
-// 					if (stateDesired == "on") {
-// 						Test.TurnOnRoomByName(room, function (error) {
-// 							Test.GWEnd();
-// 						});
-// 					}
-// 				}
+// 		if(stateDesired == "on") {
+// 			console.log("trying to turn on " + room);
+// 			Test.TurnOnRoomByName(room, function (error) {
+// 				Test.GWEnd();
 // 			});
-// 		});
+// 		} else {
+// 			console.log("trying to turn off " + room);
+// 			Test.TurnOffRoomByName(room, function (error) {
+// 				Test.GWEnd();
+// 			});
+// 		}
 // 	} else {
 // 		console.log("There was an issue initializing the token");
 // 	}
 // });
+
+// TODO: brightness
+
+//TODO if above code not working, uncomment this block and try
+Test.Init(function (error) {
+	if (!error) {
+		console.log("token is fine");
+		Test.GetState(function (error, system) {
+			Test.GetRoomStateByName(room, function (error, state, level) {
+				console.log("Room " + room + " - State " + state + " at Level " + level);
+				if (state == 1) {
+					// lights are on, if stateDesired is ON, nothing to do
+					// if stateDesired is OFF, turn lights off
+					if (stateDesired == "off") {
+						console.log("trying to turn off " + room);
+						Test.TurnOffRoomByName(room, function (error) {
+							Test.GWEnd();
+						});
+					}
+				} else {
+					// lights are off, if stateDesired is OFF, nothing to do
+					// if stateDesired is ON, turn lights on
+					if (stateDesired == "on") {
+						console.log("trying to turn on " + room);
+						Test.TurnOnRoomByName(room, function (error) {
+							Test.GWEnd();
+						});
+					}
+				}
+			});
+		});
+	} else {
+		console.log("There was an issue initializing the token");
+	}
+});
